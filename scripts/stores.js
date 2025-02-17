dbf4fbc96adb45c60d4ef7da4f217b7641099be7
SmlRat <exaltedJJ@gmail.com>
2025-02-17 16:30:26 -0500

    Filled out all location information in stores.js file

--- scripts/stores.js
+++ scripts/stores.js
@@ -1,20 +1,290 @@
 const stores = [
     {
-      "OBJECTID": 1,
-      "LICENSE_NUMBER": "21 299411 01 1L",
-      "LICENSE_TYPE": "Kennels and Pet Shops",
-      "BUSINESS_NAME": "2853914 ONTARIO INC. O/A THE URBAN ZOO",
-      "BUSINESS_ADDRESS": "33 Cromwell Crescent",
-      "COMMUNITY": "Hamilton",
-      "CATEGORY": "Pet Shop"
+        "OBJECTID": 1,
+        "LICENSE_NUMBER": "21 299411 01 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "THE URBAN ZOO",
+        "BUSINESS_ADDRESS": "540 CONCESSION ST, HAMILTON ON",
+        "COMMUNITY": "Hamilton",
+        "CATEGORY": "Pet Shop"
     },
     {
         "OBJECTID": 2,
-      "LICENSE_NUMBER": "21 299411 01 1L",
-      "LICENSE_TYPE": "Kennels and Pet Shops",
-      "BUSINESS_NAME": "2853914 ONTARIO INC. O/A THE URBAN ZOO",
-      "BUSINESS_ADDRESS": "2420 New St",
-      "COMMUNITY": "Hamilton",
-      "CATEGORY": "Pet Shop"
-    }
+        "LICENSE_NUMBER": "21 298734 03 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "4 PAWS TREE HOUSE BOARDING & DAYCARE INC",
+        "BUSINESS_ADDRESS": "636 MILLGROVE SIDEROAD, FLAMBOROUGH ON",
+        "COMMUNITY": "Flamborough",
+        "CATEGORY": "Kennel"
+    },
+    {
+        "OBJECTID": 3,
+        "LICENSE_NUMBER": "19 296849 04 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "RUFFINS PET CENTRE",
+        "BUSINESS_ADDRESS": "557 MAIN ST E, HAMILTON ON",
+        "COMMUNITY": "Hamilton",
+        "CATEGORY": "Kennel"
+      },
+      {
+        "OBJECTID": 4,
+        "LICENSE_NUMBER": "14 172152 09 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "A. GLEN SAXON KENNELS",
+        "BUSINESS_ADDRESS": "1854 TRINITY RD S, ANCASTER ON",
+        "COMMUNITY": "Ancaster",
+        "CATEGORY": "Kennel"
+      },
+      {
+        "OBJECTID": 5,
+        "LICENSE_NUMBER": "14 209622 09 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "ALL SEASONS KENNELS",
+        "BUSINESS_ADDRESS": "557 RYMAL RD E, HAMILTON ON",
+        "COMMUNITY": "Hamilton",
+        "CATEGORY": "Kennel"
+      },
+      {
+        "OBJECTID": 6,
+        "LICENSE_NUMBER": "14 244801 08 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "BINBROOK CAMP KENNELS",
+        "BUSINESS_ADDRESS": "458 HALL RD, GLANBROOK ON",
+        "COMMUNITY": "Glanbrook",
+        "CATEGORY": "Kennel"
+      },
+      {
+        "OBJECTID": 7,
+        "LICENSE_NUMBER": "15 287610 08 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "BRB K/9 OBEDIENCE O/A COOPER KENNELS",
+        "BUSINESS_ADDRESS": "226 CARLUKE RD E,  ANCASTER ON",
+        "COMMUNITY": "Hamilton",
+        "CATEGORY": "Kennel"
+      },
+      {
+        "OBJECTID": 8,
+        "LICENSE_NUMBER": "14 206749 09 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "COUNTRY HAVEN KENNELS",
+        "BUSINESS_ADDRESS": "1153 REGIONAL RD 97, FLAMBOROUGH ON",
+        "COMMUNITY": "Flamborough",
+        "CATEGORY": "Kennel"
+      },
+      {
+        "OBJECTID": 9,
+        "LICENSE_NUMBER": "15 197292 09 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "DEVONAIR KENNELS",
+        "BUSINESS_ADDRESS": "1733 CENTRE RD, FLAMBOROUGH ON",
+        "COMMUNITY": "Flamborough",
+        "CATEGORY": "Kennel"
+      },
+      {
+        "OBJECTID": 10,
+        "LICENSE_NUMBER": "14 281978 09 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "DOGS REFORMED INC.",
+        "BUSINESS_ADDRESS": "1156 HIGHLAND RD E, STONEY CREEK ON",
+        "COMMUNITY": "Stoney Creek",
+        "CATEGORY": "Kennel"
+      },
+      {
+        "OBJECTID": 11,
+        "LICENSE_NUMBER": "15 175575 09 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "FALCON LAKE KENNEL",
+        "BUSINESS_ADDRESS": "1885 MILBUROUGH LINE, FLAMBOROUGH ON",
+        "COMMUNITY": "Flamborough",
+        "CATEGORY": "Kennel"
+      },
+      {
+        "OBJECTID": 12,
+        "LICENSE_NUMBER": "15 175573 09 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "FORRESTDALE KENNELS",
+        "BUSINESS_ADDRESS": "1835 MILBUROUGH LINE, FLAMBOROUGH ON",
+        "COMMUNITY": "Flamborough",
+        "CATEGORY": "Kennel"
+      },
+      {
+        "OBJECTID": 13,
+        "LICENSE_NUMBER": "14 274858 09 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "GOODHEART KENNEL",
+        "BUSINESS_ADDRESS": "1469 SAWMILL RD, ANCASTER ON",
+        "COMMUNITY": "Ancaster",
+        "CATEGORY": "Kennel"
+      },
+      {
+        "OBJECTID": 14,
+        "LICENSE_NUMBER": "14 207490 09 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "HOLLY'S PRIDE ANCASTER INC.",
+        "BUSINESS_ADDRESS": "416 HWY 52 S, ANCASTER ON",
+        "COMMUNITY": "Ancaster",
+        "CATEGORY": "Kennel"
+      },
+      {
+        "OBJECTID": 15,
+        "LICENSE_NUMBER": "16 199386 09 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "JILMARPET KENNELS",
+        "BUSINESS_ADDRESS": "55 EIGHTH RD E, STONEY CREEK ON",
+        "COMMUNITY": "Stoney Creek",
+        "CATEGORY": "Kennel"
+      },
+      {
+        "OBJECTID": 16,
+        "LICENSE_NUMBER": "15 276661 09 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "MCCANN PROFESSIONAL DOG TRAINERS",
+        "BUSINESS_ADDRESS": "929 BROCK RD, FLAMBOROUGH ON",
+        "COMMUNITY": "Flamborough",
+        "CATEGORY": "Kennel"
+      },
+      {
+        "OBJECTID": 17,
+        "LICENSE_NUMBER": "16 220582 09 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "MCCANN PROFESSIONAL DOG TRAINERS",
+        "BUSINESS_ADDRESS": "2899 BROCK RD, FLAMBOROUGH ON",
+        "COMMUNITY": "Flamborough",
+        "CATEGORY": "Kennel"
+      },
+      {
+        "OBJECTID": 18,
+        "LICENSE_NUMBER": "15 175565 09 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "MI-PAO KENNELS REG'D",
+        "BUSINESS_ADDRESS": "702 PARKSIDE DR, FLAMBOROUGH ON",
+        "COMMUNITY": "Flamborough",
+        "CATEGORY": "Kennel"
+      },
+      {
+        "OBJECTID": 19,
+        "LICENSE_NUMBER": "19 296477 04 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "MOLLY BAY KENNELS",
+        "BUSINESS_ADDRESS": "2621 POWER LINE RD W, ANCASTER ON",
+        "COMMUNITY": "Ancaster",
+        "CATEGORY": "Kennel"
+      },
+      {
+        "OBJECTID": 20,
+        "LICENSE_NUMBER": "15 175919 08 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "NITELATCHES SHETLAND SHEEPDOGS",
+        "BUSINESS_ADDRESS": "589 EIGHTH RD E, STONEY CREEK ON",
+        "COMMUNITY": "Stoney Creek",
+        "CATEGORY": "PetShop"
+      },
+      {
+        "OBJECTID": 21,
+        "LICENSE_NUMBER": "20 298459 03 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "OLD WORLD KENNELS",
+        "BUSINESS_ADDRESS": "843 FIDDLERS GREEN RD, ANCASTER ON",
+        "COMMUNITY": "Ancaster",
+        "CATEGORY": "Kennel"
+      },
+      {
+        "OBJECTID": 22,
+        "LICENSE_NUMBER": "14 277719 09 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "PETSMART CANADA INC.",
+        "BUSINESS_ADDRESS": "21779 STONE CHURCH RD E 2, STONEY CREEK ON",
+        "COMMUNITY": "Stoney Creek",
+        "CATEGORY": "PetShop"
+      },
+      {
+        "OBJECTID": 23,
+        "LICENSE_NUMBER": "14 167524 09 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "PAULMAC'S PET STORE",
+        "BUSINESS_ADDRESS": "1565 BARTON ST E, HAMILTON ON",
+        "COMMUNITY": "Hamilton",
+        "CATEGORY": "PetShop"
+      },
+      {
+        "OBJECTID": 24,
+        "LICENSE_NUMBER": "21 298816 02 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "PET PARADISE",
+        "BUSINESS_ADDRESS": "2300 UPPER JAMES ST, GLANBROOK ON",
+        "COMMUNITY": "Glanbrook",
+        "CATEGORY": "PetShop"
+      },
+      {
+        "OBJECTID": 25,
+        "LICENSE_NUMBER": "18 293743 05 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "PETSMART CANADA INC.",
+        "BUSINESS_ADDRESS": "120 PORTIA DR, ANCASTER ON",
+        "COMMUNITY": "Ancaster",
+        "CATEGORY": "PetShop"
+      },
+      {
+        "OBJECTID": 26,
+        "LICENSE_NUMBER": "15 177792 08 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "PETSMART CANADA INC.",
+        "BUSINESS_ADDRESS": "14 MARTINDALE CRES, ANCASTER ON",
+        "COMMUNITY": "Ancaster",
+        "CATEGORY": "PetShop"
+      },
+      {
+        "OBJECTID": 27,
+        "LICENSE_NUMBER": "21 299420 01 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "PETSMART CANADA INC.",
+        "BUSINESS_ADDRESS": "502 CENTENNIAL PKWY N, HAMILTON ON",
+        "COMMUNITY": "Hamilton",
+        "CATEGORY": "PetShop"
+      },
+      {
+        "OBJECTID": 28,
+        "LICENSE_NUMBER": "15 290405 07 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "PETSMART CANADA INC.",
+        "BUSINESS_ADDRESS": "1231 BARTON ST E, HAMILTON ON",
+        "COMMUNITY": "Hamilton",
+        "CATEGORY": "PetShop"
+      },
+      {
+        "OBJECTID": 29,
+        "LICENSE_NUMBER": "14 180165 09 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "SHADOWBROOK  KENNELS",
+        "BUSINESS_ADDRESS": "107 BINBROOK RD, GLANBROOK ON",
+        "COMMUNITY": "Glanbrook",
+        "CATEGORY": "Kennel"
+      },
+      {
+        "OBJECTID": 30,
+        "LICENSE_NUMBER": "14 275269 09 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "TABARKA SHELTIE KENNELS",
+        "BUSINESS_ADDRESS": "200 14TH CON RD E, FLAMBOROUGH ON",
+        "COMMUNITY": "Flamborough",
+        "CATEGORY": "Kennel"
+      },
+      {
+        "OBJECTID": 31,
+        "LICENSE_NUMBER": "15 175555 09 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "WINDRUN FARM",
+        "BUSINESS_ADDRESS": "1279 4TH CON RD W, FLAMBOROUGH ON",
+        "COMMUNITY": "Flamborough",
+        "CATEGORY": "Kennel"
+      },
+      {
+        "OBJECTID": 32,
+        "LICENSE_NUMBER": "15 175554 09 1L",
+        "LICENSE_TYPE": "Kennels and Pet Shops",
+        "BUSINESS_NAME": "WYLDEWOOD KENNELS",
+        "BUSINESS_ADDRESS": "231 11TH CON RD E, FLAMBOROUGH",
+        "COMMUNITY": "Flamborough",
+        "CATEGORY": "Kennel"
+      },
 ]
