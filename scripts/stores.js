9d81c3ce1b9cb308788388d778ed00b88039badd
SmlRat <exaltedJJ@gmail.com>
2025-02-17 15:05:41 -0500

    google map api key updated and is now functional

---
+++ scripts/stores.js
@@ -0,0 +1,11 @@
+const stores = [
+    {
+      "OBJECTID": 1,
+      "LICENSE_NUMBER": "21 299411 01 1L",
+      "LICENSE_TYPE": "Kennels and Pet Shops",
+      "BUSINESS_NAME": "2853914 ONTARIO INC. O/A THE URBAN ZOO",
+      "BUSINESS_ADDRESS": "540 CONCESSION ST  HAMILTON ON",
+      "COMMUNITY": "Hamilton",
+      "CATEGORY": "Pet Shop",
+    }
+]
