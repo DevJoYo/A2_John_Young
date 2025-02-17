bda1682af5a8fcc22cbaa325f7107c86466b2ff7
SmlRat <exaltedJJ@gmail.com>
2025-02-17 15:46:36 -0500

    marker functionality implemented

--- scripts/stores.js
+++ scripts/stores.js
@@ -4,8 +4,17 @@ const stores = [
       "LICENSE_NUMBER": "21 299411 01 1L",
       "LICENSE_TYPE": "Kennels and Pet Shops",
       "BUSINESS_NAME": "2853914 ONTARIO INC. O/A THE URBAN ZOO",
-      "BUSINESS_ADDRESS": "540 CONCESSION ST  HAMILTON ON",
+      "BUSINESS_ADDRESS": "33 Cromwell Crescent",
       "COMMUNITY": "Hamilton",
-      "CATEGORY": "Pet Shop",
+      "CATEGORY": "Pet Shop"
+    },
+    {
+        "OBJECTID": 2,
+      "LICENSE_NUMBER": "21 299411 01 1L",
+      "LICENSE_TYPE": "Kennels and Pet Shops",
+      "BUSINESS_NAME": "2853914 ONTARIO INC. O/A THE URBAN ZOO",
+      "BUSINESS_ADDRESS": "2420 New St",
+      "COMMUNITY": "Hamilton",
+      "CATEGORY": "Pet Shop"
     }
 ]
